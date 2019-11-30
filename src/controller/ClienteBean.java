08bd9cff7eaa2dc10fbc8b98ce5add541b9dcaa2
felipealvss <lipealvessax@gmail.com>
2019-11-29 23:35:20 -0300

    More updates on ClienteBean and Livro bean, SQL definitions and other stufs

--- src/controller/ClienteBean.java
+++ src/controller/ClienteBean.java
@@ -1,15 +1,29 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Map;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.annotation.ManagedBean;
 
-@ManagedBean(value="clienteBean")
+@ManagedBean(name="clienteBean")
+@RequestScoped
 public class ClienteBean {
 
-	private Integer id;
-	private String nome;
-	private String idade;
-	private String contato;
-	private String email;
+	public Integer id;
+	public String nome;
+	public String idade;
+	public String contato;
+	public String email;
+	public ArrayList clienteList;
+	private Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+	Connection connection;
 	
 	public Integer getId() {
 		return id;
@@ -42,4 +56,101 @@ public class ClienteBean {
 		this.email = email;
 	}
 	
+	// Definir conexão SQL
+	public Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cliente","root","1234");
+		} catch(Exception e){
+			System.out.print(e);
+		}
+		return connection;
+	}
+		
+	// Pegar todos os registros de Clientes
+	public ArrayList clienteList() {
+		try {
+			clienteList = new ArrayList();
+			connection = getConnection();
+			Statement stmt = getConnection().createStatement();
+			ResultSet rs = stmt.executeQuery("select * from cliente");
+			while(rs.next()) {
+				ClienteBean cliente = new ClienteBean();
+				cliente.setId(rs.getInt("id"));
+				cliente.setNome(rs.getNString("nome"));
+				cliente.setIdade(rs.getNString("idade"));
+				cliente.setContato(rs.getNString("contato"));
+				cliente.setEmail(rs.getNString("email"));
+				clienteList.add(cliente);
+			}
+			connection.close();
+		}catch(Exception e) {
+			System.out.print(e);
+		}
+		return clienteList;
+	}
+	
+	// Salvar registros de Clientes
+	public String save() {
+		int resultado = 0;
+		try {
+			connection = getConnection();
+			PreparedStatement stmt = connection.prepareStatement("insert into cliente(id, nome, idade, contato, email) values(?,?,?,?,?)");
+			stmt.setInt(1, id);
+			stmt.setString(2, nome);
+			stmt.setString(3, idade);
+			stmt.setString(4, contato);
+			stmt.setString(5, email);
+			result = stmt.executeUpdate();
+			connection.close();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		if(result != 0) {
+			return "cadastroCliente.xhtml?faces-redirect-true";
+		}else {
+			return "cadastrarCliente.xhtml?faces.redirect-true";
+		}
+	}
+	
+	// Pegar registro de CLiente
+	public String edit() {
+		ClienteBean cli = null;
+		System.out.println(id);
+		try {
+			connection = getConnection();
+			Statement stmt = getConnection().createStatement();
+			ResultSet rs = stmt.executeQuery("select * from cliente where id = " + (id));
+			rs.next();
+			cli = new ClienteBean();
+			cli.setId(rs.getInt("id"));
+			cli.setNome(rs.getString("nome"));
+			cli.setIdade(rs.getString("idade"));
+			cli.setContato(rs.getString("contato"));
+			cli.setEmail(rs.getString("email"));
+			sessionMap.put("editCliente", cli);
+			connection.close();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		return "cadastrarCliente.xhtml?faces-redirect-true";
+	}
+	
+	//Atualizar Registro de Cliente
+	public String update(ClienteBean c) {
+		try {
+			connection = getConnection();
+			PreparedStatement stmt = connection.prepareStatement("update cliente set nome = ?, idade = ?, contato = ?, email = ? where id = ?");
+			stmt.setString(1, c.getNome());
+			stmt.setString(2, c.getIdade());
+			stmt.setString(3, c.getContato());
+			stmt.setString(4, c.getEmail());
+			stmt.setInt(5, c.getId());
+			stmt.executeUpdate();
+			connection.close();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		return "index.xhtml?faces-redirect-true";
+	}
 }
--- src/controller/LivroBean.java
+++ src/controller/LivroBean.java
@@ -1,16 +1,30 @@
 package controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Map;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import javax.annotation.ManagedBean;
 
-@ManagedBean(value="livroBean")
+@ManagedBean(name="livroBean")
+@RequestScoped
 public class LivroBean {
 
-	private Integer id;
-	private String nome;
-	private String editora;
-	private String ano;
-	private String descricao;
-
+	public Integer id;
+	public String nome;
+	public String editora;
+	public String ano;
+	public String descricao;
+	public ArrayList livroList;
+	private Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+	Connection connection;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -51,5 +65,101 @@ public class LivroBean {
 		this.descricao = descricao;
 	}
 	
+	// Definir conexão SQL
+	public Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/livro","root","1234");
+		} catch(Exception e){
+			System.out.print(e);
+		}
+		return connection;	
+	}
 	
+	// Pegar todos os registros dos Livros
+	public ArrayList livroList() {
+		try {
+			livroList = new ArrayList();
+			connection = getConnection();
+			Statement stmt = getConnection().createStatement();
+			ResultSet rs = stmt.executeQuery("select * from livro");
+			while(rs.next()) {
+				LivroBean livro = new LivroBean();
+				livro.setId(rs.getInt("id"));
+				livro.setNome(rs.getNString("nome"));
+				livro.setEditora(rs.getNString("editora"));
+				livro.setAno(rs.getNString("ano"));
+				livro.setDescricao(rs.getNString("descricao"));
+				livroList.add(livro);
+			}
+			connection.close();
+		}catch(Exception e) {
+			System.out.print(e);
+		}
+		return livroList;
+	}
+	
+	// Salvar registros dos Livros
+	public String save() {
+		int resultado = 0;
+		try {
+			connection = getConnection();
+			PreparedStatement stmt = connection.prepareStatement("insert into livro(id, nome, editora, ano, descricao) values(?,?,?,?,?)");
+			stmt.setInt(1, id);
+			stmt.setString(2, nome);
+			stmt.setString(3, editora);
+			stmt.setString(4, ano);
+			stmt.setString(5, descricao);
+			result = stmt.executeUpdate();
+			connection.close();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		if(result != 0) {
+			return "cadastroLivro.xhtml?faces-redirect-true";
+		}else {
+			return "cadastrarLivro.xhtml?faces.redirect-true";
+		}
+	}
+	
+	// Pegar registro de Livro
+	public String edit() {
+		LivroBean liv = null;
+		System.out.println(id);
+		try {
+			connection = getConnection();
+			Statement stmt = getConnection().createStatement();
+			ResultSet rs = stmt.executeQuery("select * from livro where id = " + (id));
+			rs.next();
+			liv = new LivroBean();
+			liv.setId(rs.getInt("id"));
+			liv.setNome(rs.getString("nome"));
+			liv.setEditora(rs.getString("editora"));
+			liv.setAno(rs.getString("ano"));
+			liv.setDescricao(rs.getString("descricao"));
+			sessionMap.put("editLivro", liv);
+			connection.close();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		return "cadastrarLivro.xhtml?faces-redirect-true";
+	}
+	
+	// Atualizar Registro de Livro
+	public String update(LivroBean l) {
+		try {
+			connection = getConnection();
+			PreparedStatement stmt = connection.prepareStatement("update cliente set nome = ?, editora= ?, ano = ?, descricao = ? where id = ?");
+			stmt.setString(1, l.getNome());
+			stmt.setString(2, l.getEditora());
+			stmt.setString(3, l.getAno());
+			stmt.setString(4, l.getDescricao());
+			stmt.setInt(5, l.getId());
+			stmt.executeUpdate();
+			connection.close();
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		return "index.xhtml?faces-redirect-true";
+	}
 }
